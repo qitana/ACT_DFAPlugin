d33b5814ddc6a1d1d9b3d18f8df496edeb0332fd
qitana <53415856+qitana@users.noreply.github.com>
2019-12-02 00:11:52 +0900

    combine build scripts

--- distribute.ps1
+++ distribute.ps1
@@ -1,5 +1,24 @@
 ﻿# ビルド
-./build.bat
+$msbuild = $null
+$msbuild_exe = @(
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe", 
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe", 
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe",
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe")
+foreach ($m in $msbuild_exe) {
+    if (Test-Path $m) {
+        $msbuild = $m
+        break
+    }
+}
+if ($null -eq $msbuild) {
+    Write-Output ("MSBuild.exe が見つかりませんでした。Visual Studio または Build Tools をインストールしてください。")
+	pause
+	return
+}
+& $msbuild DFAPlugin.sln /nologo /v:minimal /t:Clean /p:Configuration=Release /p:Platform="Any CPU" | Write-Output
+& $msbuild DFAPlugin.sln /nologo /v:minimal /t:Restore /p:Configuration=Release /p:Platform="Any CPU" | Write-Output
+& $msbuild DFAPlugin.sln /nologo /v:minimal /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" | Write-Output
 
 # バージョン取得
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(".\DFAPlugin\bin\Release\DFAPlugin.dll").FileVersion
